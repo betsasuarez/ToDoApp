1b46c93abe801198569683a89f05dff3c4e27548
CORREA <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2023-10-24 21:09:15 -0500

    Bingding

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace ToDoApp
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoApp
 {
     public partial class MainPage : ContentPage
     {
+        public string MyTitle { get; set; } = "Lista de Tareas";
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
-    }
+
+        private void AddBnt_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "Agregado", "ok");
+        }
+
+    }   
+
+
 }
